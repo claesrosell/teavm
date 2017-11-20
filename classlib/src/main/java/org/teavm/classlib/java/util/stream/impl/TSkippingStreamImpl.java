cbf47a2c624876916bea0a923bfac2cdb09c70ab
Alexey Andreev <konsoletyper@gmail.com>
2017-11-20 23:03:18 +0300

    ~fixup 9a0c6b2d87c9a916a61037fb29acd8f6cab6bed1

--- classlib/src/main/java/org/teavm/classlib/java/util/stream/impl/TSkippingStreamImpl.java
+++ classlib/src/main/java/org/teavm/classlib/java/util/stream/impl/TSkippingStreamImpl.java
@@ -26,6 +26,7 @@ public class TSkippingStreamImpl<T> extends TSimpleStreamImpl<T> {
     public TSkippingStreamImpl(TSimpleStreamImpl<T> sourceStream, int skip) {
         this.sourceStream = sourceStream;
         this.skip = skip;
+        remaining = skip;
     }
 
     @Override
